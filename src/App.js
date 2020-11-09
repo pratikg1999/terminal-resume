97e539a685dfec0d7fc082fca64be7a4cdb8fe13
Pratik Gupta <pratik17100@iiitnr.edu.in>
2020-11-09 12:21:30 +0530

    input span is focused on click anywhere down

--- src/App.js
+++ src/App.js
@@ -12,12 +12,12 @@ class App extends React.Component {
     let [command, ...args] = inp.split(' ');
     let newCurDir = this.state.curDir;
     if (args.length > 1) {
-      return {output:"Invalid command"};
+      return { output: "Invalid command" };
     }
-    else if(args.length === 0){
+    else if (args.length === 0) {
       newCurDir = "/home";
     }
-    else{
+    else {
       args = args[0].trim();
       if (args === "..") {
         newCurDir = this.state.curDir.substring(0, this.state.curDir.lastIndexOf("/")) || "/";
@@ -26,17 +26,17 @@ class App extends React.Component {
         newCurDir = args;
       }
       else {
-        if(args[args.length-1] === "/"){
-          args = args.substr(0, args.length-1);
+        if (args[args.length - 1] === "/") {
+          args = args.substr(0, args.length - 1);
         }
         if (args[0] === "/") {
           newCurDir = args;
         }
-        else{
-          if(newCurDir === "/"){
+        else {
+          if (newCurDir === "/") {
             newCurDir = this.state.curDir + args;
           }
-          else{
+          else {
             newCurDir = this.state.curDir + "/" + args;
           }
         }
@@ -47,7 +47,7 @@ class App extends React.Component {
     //     curDir : newCurDir,
     //   }
     // })
-    return {output: "Changing dir to "+newCurDir, stateVars:{curDir: newCurDir}};
+    return { output: "Changing dir to " + newCurDir, stateVars: { curDir: newCurDir } };
   }
 
   constructor(props) {
@@ -55,15 +55,15 @@ class App extends React.Component {
     // delete props.commands["help"]; // not-working
     console.log(props.commands);
     props.commands["cd"] = this.fun_cd;
-    let fun_help = (inp)=>{
+    let fun_help = (inp) => {
       let res = "Enter one of the following commands with optional args:\n";
-      for(let command in props.commands){
-        if(command!=="help"){
-          res+=command+"\n";
+      for (let command in props.commands) {
+        if (command !== "help") {
+          res += command + "\n";
         }
       }
-      res+="help";
-      return {output:res};
+      res += "help";
+      return { output: res };
     }
     props.commands["help"] = fun_help;
     if (props.invalidOutput === undefined) {
@@ -118,11 +118,11 @@ class App extends React.Component {
     // const newInput = this.newInpRef.current.textContent;
     const newCommand = newInput.split(' ')[0];
     let newOut, stateVars, callback;
-    if(this.props.commands[newCommand] === undefined){
+    if (this.props.commands[newCommand] === undefined) {
       newOut = this.invalidOutput;
     }
-    else{
-      ({output:newOut, stateVars, callback} = this.props.commands[newCommand](newInput));
+    else {
+      ({ output: newOut, stateVars, callback } = this.props.commands[newCommand](newInput));
     }
     // if (newOut === undefined) {
     //   newOut = this.invalidOutput;
@@ -140,7 +140,7 @@ class App extends React.Component {
         history: newHist,
       }
     });
-    if(callback){
+    if (callback) {
       callback();
     }
   }
@@ -153,10 +153,10 @@ class App extends React.Component {
             return (
               <div className="group" key={ind}>
                 <p className="first-line">
-                  <span className="username" style={{color:this.state.userSysColor}}>{this.state.username}@</span>
-                  <span className="system" style={{color:this.state.userSysColor}}>{this.state.system}</span>
+                  <span className="username" style={{ color: this.state.userSysColor }}>{this.state.username}@</span>
+                  <span className="system" style={{ color: this.state.userSysColor }}>{this.state.system}</span>
                   <span>:</span>
-                  <span className="curDir" style={{color:this.state.curDirColor}}>{hist.curDir}</span>
+                  <span className="curDir" style={{ color: this.state.curDirColor }}>{hist.curDir}</span>
                   <span className="prompt">{hist.prompt} </span>
                   <span className="inp">{hist.inp}</span>
                 </p>
@@ -167,19 +167,19 @@ class App extends React.Component {
             )
           })}
         </div>
-        <div className="new">
-          <p className="first-line">
-            <span className="username" style={{color:this.state.userSysColor}}>{this.state.username}@</span>
-            <span className="system" style={{color:this.state.userSysColor}}>{this.state.system}</span>
+        <div className="new" onClick={(e)=>{console.log("cliked", e);this.newInpRef.current.focus();}}>
+          <div className="left-prompts" onClick={(e)=>e.stopPropagation()}>
+            <span className="username" style={{ color: this.state.userSysColor }}>{this.state.username}@</span>
+            <span className="system" style={{ color: this.state.userSysColor }}>{this.state.system}</span>
             <span>:</span>
-            <span className="curDir" style={{color:this.state.curDirColor}}>{this.state.curDir}</span>
+            <span className="curDir" style={{ color: this.state.curDirColor }}>{this.state.curDir}</span>
             <span className="prompt">{this.state.prompt} </span>
-            <span id="new-inp" ref={this.newInpRef} contentEditable="true" onKeyDown={(e) => {
-              if (e.key === "Enter") {
-                this.takeNewInput(e.target.textContent);
-              }
-            }}></span>
-          </p>
+          </div>
+          <span id="new-inp" ref={this.newInpRef} contentEditable="true" onKeyUp={(e) => {
+            if (e.key === "Enter") {
+              this.takeNewInput(e.target.textContent);
+            }
+          }}></span>
         </div>
       </div>
     )
